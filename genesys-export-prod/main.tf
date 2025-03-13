8275ff1370bc9eaed1fc228c22b66d3d058e4385
razarehman990 <136132490+razarehman990@users.noreply.github.com>
2025-03-13 13:42:50 -0600

    Update main.tf

--- genesys-export-prod/main.tf
+++ genesys-export-prod/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 variable "aws_region" {
   type        = string
-  description = "AWS region for Genesys Cloud (e.g. 'us-east-1')"
+  description = "AWS region for Genesys Cloud (e.g. 'ca-central-1')"
 }
 
 provider "genesyscloud" {
@@ -17,11 +17,11 @@ provider "genesyscloud" {
 }
 
 resource "genesyscloud_tf_export" "full_export" {
-  directory                    = "../export/prod"  # Exports to export/prod folder
-  include_filter_resources     = []                # Empty list means export all supported resources
+  directory                    = "../export/prod"  # Export files to export/prod folder
+  include_filter_resources     = []                # Export all supported resource types
   export_as_hcl                = true              # Export in HCL format
-  include_state_file           = false             # Do not include the tfstate
-  split_files_by_resource      = false             # Set to true for splitting by resource type
-  enable_dependency_resolution = true              # Automatically include dependencies
+  include_state_file           = false             # Exclude the state file for portability
+  split_files_by_resource      = false             # Set to true to split files by resource type
+  enable_dependency_resolution = true              # Include dependencies automatically
   log_permission_errors        = true              # Log permission errors instead of failing
 }
